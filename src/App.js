270a6ebedff7a0e98c89aa1033a35c3a58de4b2a
chamibia <b.chami39@gmail.com>
2022-03-11 22:08:10 -0500

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Nav></Nav>
       <main>
         <About></About>
+        <Portfolio></Portfolio>
+        <ContactForm></ContactForm>
       </main>
     </div>
   );
