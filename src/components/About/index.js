9c56f9bb8b104939474c1b305ec5b9c45bd74b51
chamibia <b.chami39@gmail.com>
2022-03-12 20:01:48 -0500

    added about section, added projects images

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import aboutImage from "../../assets/about/about.jpg";
 
 
 
 function About() {
   return (
     <div>
-      <h1>B CHAMI</h1>
+      <h1 id="about">B CHAMI</h1>
       <div>
-        <p>
-          
-        </p>
+      <img src={aboutImage} className="my-2" style={{width:"100%", height:"20%"}} alt="cover" />
       </div>
+      <p> Bianca Chami is a soon to be full stack developer. She is currently completing a certificate at the University of Toronto's Full Stack Developer bootcamp. You can follow her on github here: <a href="https://github.com/chamibia"> chamibia</a></p>
+
     </div>
   )
 
 }
 
 export default About;
+
+
+
+
+
+ 
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import artistLyrics from '../../assets/portfolio/artist.png';
+import calendar from '../../assets/portfolio/calendar.png';
+import maintaker from '../../assets/portfolio/maintaker.png';
+import weather from '../../assets/portfolio/weatherD.png';
+import workday from '../../assets/portfolio/workday.png';
 
 function Projects() {
 
@@ -6,12 +11,14 @@ function Projects() {
         {
             name: 'P2G2 JobTracker',
             description: 'Job tracker that updates status for employees',
+            img: calendar,
             repository: 'https://github.com/NG-Vincent/P2G2-JobTracker',
             link: 'https://safe-cove-20593.herokuapp.com/'
         },
         {
             name: 'Lyrics Finder',
             description: 'App to search for artist lyrics, created with iTunes and lyrics.ovh APIs',
+            img: artistLyrics,
             repository: 'https://github.com/chamibia/lyrics-finder',
             link: 'https://chamibia.github.io/lyrics-finder/'
 
@@ -19,6 +26,7 @@ function Projects() {
         {
             name: 'Note Taker',
             description: 'Take notes, made with express.js',
+            img: maintaker,
             repository: 'https://github.com/chamibia/note-taker',
             link: 'https://taker--note.herokuapp.com/'
 
@@ -26,6 +34,7 @@ function Projects() {
         {
             name: 'Work Day Scheduler',
             description: 'Keep track of your workday with this scheduler',
+            img: workday,
             repository: 'https://github.com/chamibia/scheduler', 
             link: 'https://chamibia.github.io/scheduler/'
 
@@ -33,6 +42,7 @@ function Projects() {
         {
             name: 'Weather Dashboard',
             description: 'A weather dashboard that allows you to search and save multiple cities and returns a 5-day forcast',
+            img: weather,
             repository: 'https://github.com/chamibia/weather-dashboard',
             link: 'https://chamibia.github.io/weather-dashboard/'
 
@@ -45,6 +55,7 @@ function Projects() {
                 return (
                     <div key={project.name}>
                         <h1 className="proj-name">{project.name}</h1>
+                        <img src={project.img }className="img-fluid rounded-start" alt="project" />
                         <h2 className="proj-desc">{project.description}</h2>
                         <p className="proj-repo"><a href={project.repository}>Github</a></p>
                         <p className="proj-link"><a href={project.link}>Project link</a></p>
